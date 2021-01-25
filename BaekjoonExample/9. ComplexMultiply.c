d0973f7ad9f8ae6fa80c73263c190f49e94b7be2
Yapmong <khj566977@naver.com>
2021-01-26 00:28:35 +0900

    deleteUnreal/BaekjoonEx9

---
+++ BaekjoonExample/9. ComplexMultiply.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	int hund, ten, one;
+	int result;
+
+	scanf("%d%d", &a, &b);
+	hund = b / 100;
+	b = b - (b / 100 * 100);
+	ten = b / 10;
+	b = b - (b / 10 * 10);
+	one = b / 1;
+	printf("%d\n%d\n%d\n%d", a * one, a * ten, a * hund, a * b);
+	return 0;
+}
