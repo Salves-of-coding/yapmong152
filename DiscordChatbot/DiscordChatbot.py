3c214011dbb6af6bca253692d39ab6745d37d1e2
Yapmong <khj566977@naver.com>
2021-04-12 18:04:03 +0900

    Friend

--- DiscordChatbot/DiscordChatbot.py
+++ DiscordChatbot/DiscordChatbot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
 
 token = 'ODI3MTI4NjE2NTAwMzMwNTI3.YGWhjQ.iNHhY3sJ9VjXqNweUscaYJ71QCM'
 
@@ -12,12 +14,29 @@ async def on_ready():
     print("READY")
 
 @client.command(name='씹섭아')
-async def common(ctx,command):
+async def common(ctx,command,seccommand=''):
     if command == '도움':
-        await ctx.send(f'추후 추가 예정')
+        await ctx.send(f'```cs\n# 씹섭봇 사용법'
+                       f'\n명령어 목록만 보려면 \'씹섭아 명령어\'를 쳐주세요'
+                       f'\n\n\"1.명령어 사용방법\"'
+                       f'\n아래 목록에 있는 명령어들을 씹섭아 또는 씹섭이 뒤에 적어서 사용합니다.'
+                       f'\n\n\"2.씹섭아 명령어 목록\"'
+                       f'\n안녕-씹섭이와 인사를 주고받습니다.'
+                       f'\n\n\"3.씹섭이 명령어 목록\"'
+                       f'\n병신-씹섭이에게 시비를 겁니다.```')
 
     if command == '안녕':
         await ctx.send(f'ㅎㅇ')
+
+    if command == '오스':
+        req = Request(f'{seccommand}')
+        res = urlopen(req)
+        html = res.read()
+
+        bs = BeautifulSoup(html, 'html.parser')
+        tags = bs.find('div', attrs={'class': 'value-display__value'})
+        print(tags.text)
+        await ctx.send(tags.text)
 
 @client.command(name='씹섭이')
 async def common(ctx,command):
---
+++ 도움/1..py
@@ -0,0 +1,6 @@
+l = ['KAU','kau','Korea','CY','BTS','KAI','iu','TaylorSwift','jennie','NaSA','LoL']
+for i in l:
+    if i.isupper() == True:
+        l.remove(i)
+
+print (l)
